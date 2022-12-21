3060c77a15aa3848248b2b07ba93908cecbbbd6d
Harsh <74731135+harshturna@users.noreply.github.com>
2022-12-21 01:11:52 -0600

    Set the setTimeout to run every 1s (from 800ms)

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ const fx = new TextScramble(el);
 let counter = 0;
 const next = () => {
   fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 800);
+    setTimeout(next, 1000);
   });
   counter = (counter + 1) % phrases.length;
 };
